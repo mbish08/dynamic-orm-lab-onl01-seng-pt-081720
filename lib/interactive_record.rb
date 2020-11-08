533371f1d2167dad9f62c4c61ca410c2ce93e653
Mikel Bishop <mdrew8@gmail.com>
2020-11-08 00:25:52 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,4 +56,16 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
   
+  def self.find_by(attribute)
+    column_name = attribute.keys[0].to_s
+    value_name = attribute.values[0]
+
+    sql = <<-SQL
+      SELECT * FROM #{table_name}
+      WHERE #{column_name} = ?
+      SQL
+
+    DB[:conn].execute(sql, value_name);
+  end
+  
 end
